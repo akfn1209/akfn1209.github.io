33f80faf9142cecbb5fc5d68c7ba524d3a3dcc25
akfn1209 <81076764+akfn1209@users.noreply.github.com>
2021-11-22 13:33:11 +0900

    Add files via upload

---
+++ precache-manifest.709b0e9eee38f789f3f190b2c71c8b5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3728f099d6c8f76e7cd5",
+    "url": "/css/app.089ea4d3.css"
+  },
+  {
+    "revision": "f338c4fcc7835714efde561ec3ecc075",
+    "url": "/follower.json"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "80da951c3bcfa1d5bfef6fd488c72eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3728f099d6c8f76e7cd5",
+    "url": "/js/app.d11d96de.js"
+  },
+  {
+    "revision": "3332ab5e3d97e19afb6c",
+    "url": "/js/chunk-vendors.f8601025.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
